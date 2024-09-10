31048db750ec1ee56ef64287f3f2b7f22b4853bc
李哲楷 <terry20070415@gmail.com>
2024-09-10 23:31:09 +0800

    Fix data generation bug

--- generateblankdata.py
+++ generateblankdata.py
@@ -17,10 +17,11 @@ data={"color": [
 with open(setting_jsonpath, 'r', encoding='utf-8') as file:
         setting = json.load(file)
 
-for i in range(len(setting["dancers"])):
+for i in range(len(setting["dancersname"])):
         data["frames"].append([])
         data["frames"][i].append([])
-        for j in range(len(setting["dancers"][i])):
+        print(setting["dancersname"][i])
+        for j in range(len(setting["dancers"][setting["dancersname"][i]]["parts"])):
             
             data["frames"][i][0].append(0)
 with open(data_jsonpath, 'w', encoding='utf-8') as file:
